7bed622c0ca6975b044ac44782f049056dadb8b0
Raphael De Freitas <raphael@de-freitas.net>
2017-08-19 12:55:22 +0200

    Set the default baseUrl

--- src/configuration/configuration.ts
+++ src/configuration/configuration.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Configuration {
-    public baseUrl;
+    public baseUrl = 'https://api.adrenalivre.com';
 
     constructor(data: any = {}) {
         Object.assign(this, data);
