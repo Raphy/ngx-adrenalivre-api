40de31150dcdd5412eab9bbf7ad8e06de7d3ff9d
Raphael De Freitas <raphael@de-freitas.net>
2017-09-24 16:41:46 +0200

    full link or download contents

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -3,15 +3,26 @@ import { DomSanitizer } from '@angular/platform-browser';
 
 import { FileRepository } from "./file.repository";
 import { File } from "./file";
+import { AuthService } from "../auth";
+import { Session } from "../session/session";
+import { Configuration } from "../configuration";
 
 @Injectable()
 export class FileService {
-    constructor(private fileRepository: FileRepository, private domSanitizer: DomSanitizer){
+    constructor(private fileRepository: FileRepository, private domSanitizer: DomSanitizer, private configuration: Configuration, private authService: AuthService) {
     }
 
-    updateUrl(file: File) {
+    updateUrl(file: File, download: boolean = false) {
         if (file && file.id && file.url === null) {
-            this.fileRepository.getContents(file).subscribe((blob: Blob) => file.url = this.domSanitizer.bypassSecurityTrustResourceUrl(window.URL.createObjectURL(blob)));
+            if (download) {
+                this.fileRepository.getContents(file).subscribe((blob: Blob) => file.url = this.domSanitizer.bypassSecurityTrustResourceUrl(window.URL.createObjectURL(blob)));
+            } else {
+                this.authService.getSession().subscribe((session: Session | Error) => {
+                    if (session instanceof Session) {
+                        file.url = this.configuration.baseUrl + '/' + file.id + '/contents?session=' + session.id
+                    }
+                });
+            }
         }
     }
 }
