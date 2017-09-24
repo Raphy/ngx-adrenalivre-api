8d40a4d85ac9b5c809429d9723e31d37cbb0e89b
Raphael De Freitas <raphael@de-freitas.net>
2017-09-24 23:44:39 +0200

    add name

--- src/credit-offer/credit-offer.ts
+++ src/credit-offer/credit-offer.ts
@@ -1,7 +1,12 @@
 export class CreditOffer {
     public id: string;
+
+    public name: string;
+
     public credits: number;
+
     public price: number;
+
     public currency: string;
 
     constructor(data: any = {}) {
