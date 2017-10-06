39f082f7490bc7590dbd4e2bd15add3d51f08242
Raphael De Freitas <raphael@de-freitas.net>
2017-10-06 16:46:54 +0200

    debug

--- src/repository.ts
+++ src/repository.ts
@@ -6,8 +6,7 @@ import { Configuration } from './configuration';
 import { Error, ErrorFactory } from './error';
 import { Model } from "./model";
 
-export abstract class Repository<T extends Model>
-{
+export abstract class Repository<T extends Model> {
     constructor(protected http: AuthHttp, protected configuration: Configuration) {
     }
 
@@ -19,6 +18,11 @@ export abstract class Repository<T extends Model>
         return this.http.get(this.configuration.baseUrl + '/' + this.getEndpoint(null), {params: params})
             .map((response: Response) => response.json())
             .map((data: object[]) => data.map((itemData) => this.createItem(itemData)))
+            .do((res) => {
+                if (this.configuration.debug) {
+                    console.log('Repository List', res);
+                }
+            })
             .catch((errorCaught: any) => {
                 const error = ErrorFactory.create(errorCaught);
                 if (error) {
@@ -33,6 +37,11 @@ export abstract class Repository<T extends Model>
         return this.http.get(this.configuration.baseUrl + '/' + this.getEndpoint(null) + '/' + id)
             .map((response: Response) => response.json())
             .map((itemData: object) => this.createItem(itemData))
+            .do((res) => {
+                if (this.configuration.debug) {
+                    console.log('Repository Retrieve', res);
+                }
+            })
             .catch((errorCaught: any) => {
                 const error = ErrorFactory.create(errorCaught);
                 if (error) {
@@ -48,6 +57,11 @@ export abstract class Repository<T extends Model>
             return this.http.patch(this.configuration.baseUrl + '/' + this.getEndpoint(item) + '/' + item.id, item.toForm())
                 .map((response: Response) => response.json())
                 .map((data: object) => item.hydrate(data))
+                .do((res) => {
+                    if (this.configuration.debug) {
+                        console.log('Repository Save', res);
+                    }
+                })
                 .catch((errorCaught: any) => {
                     const error = ErrorFactory.create(errorCaught);
                     if (error) {
@@ -74,6 +88,11 @@ export abstract class Repository<T extends Model>
     remove(item: T): Observable<void | Error> {
         return this.http.delete(this.configuration.baseUrl + '/' + this.getEndpoint(null) + '/' + item.id)
             .map((response: Response) => null)
+            .do(() => {
+                if (this.configuration.debug) {
+                    console.log('Repository Delete', item);
+                }
+            })
             .catch((errorCaught: any) => {
                 const error = ErrorFactory.create(errorCaught);
                 if (error) {
