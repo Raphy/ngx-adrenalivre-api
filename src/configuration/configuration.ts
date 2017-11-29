45e65381a62e9e7fb8ec6e687287f4d845f52f9e
Raphael De Freitas <raphael@de-freitas.net>
2017-11-29 12:31:30 +0100

    Add debug

--- src/configuration/configuration.ts
+++ src/configuration/configuration.ts
@@ -4,6 +4,8 @@ import { Injectable } from '@angular/core';
 export class Configuration {
     baseUrl: string = 'https://api.adrenalivre.com';
 
+    debug: boolean = false;
+
     constructor(data: any = {}) {
         Object.assign(this, data);
     }
