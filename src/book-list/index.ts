7c0304af4ce1a042f2a468d4423ac6cff493334a
Raphael De Freitas <raphael@de-freitas.net>
2018-01-15 18:05:12 +0100

    add bl items

--- src/book-list/index.ts
+++ src/book-list/index.ts
@@ -3,3 +3,5 @@ export * from './book-list.repository';
 export * from './book-list';
 export * from './book-list-factory';
 export * from './volume-book-list';
+export * from './volume-book-list-item';
+export * from './collection-book-list-item';
